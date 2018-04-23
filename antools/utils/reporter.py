a11b00862838a87c1d5a6ecaf8192756cba3e89c
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2018-04-23 23:55:23 +0100

    first things added for the reporter

---
+++ antools/utils/reporter.py
@@ -0,0 +1,103 @@
+"""Reporter for training runs
+
+This class can be used to store statistics related to the trainign of a
+classifier. The generated report can be dumped into a JSON file whenever
+the training complete.
+
+A run is a collection of parameters and iterations. The parameters can be added
+individually or in bulk, with a dictionary. Anyway, a run should be initialized
+first.
+"""
+
+import datetime
+import time
+import json
+import collections
+
+import humanize
+
+
+class Reporter():
+
+    def __init__(self, destination):
+        self.destination = destination
+        self.report = collections.OrderedDict()
+        self.__run = False
+
+    def spawn_run(self, run_name):
+        """Spawn a run within the report
+
+        This method initializes a run within the report. The run name is
+        required.
+
+        Arguments:
+
+        run_name -- the name of the run to create
+        """
+        self.report['run_name'] = run_name
+        self.report['start_time'] = datetime.datetime.now()
+        self.report['params'] = {}
+        self.__run = True
+
+    def add_run_param(self, k, v):
+        """Add new run parameter
+
+        This method creates a new parameter for the run. The parameter will be
+        included in the 'params' dictionary within the report.
+
+        Arguments:
+
+        k -- the key, that is, the name of the new parameter
+        v -- the value, that is, the value of the new parameter
+        """
+        self.__validate_run()
+        self.report['params'][k] = v
+
+    def add_run_params(self, d):
+        """Add set of run parameters
+
+        This method adds run parameters in bulk, by taking them from the
+        provided dictionary.
+
+        Arguments:
+
+        d -- the dictionary to copy within the params dictionary
+        """
+        for param in d:
+            self.add_run_param(param, d[param])
+
+    def add_iteration(self, iteration):
+        self.__validate_run()
+
+        if self.report.get('iterations') is None:
+            self.report['iterations'] = []
+
+        end = datetime.datetime.now()
+        iteration['end_time'] = end
+
+        if len(self.report['iterations']) > 0:
+            start = self.report['iterations'][-1]['end_time']
+        else:
+            start = self.report['start_time']
+
+        iteration['elapsed'] = end - start
+        iteration['elapsed_natural'] = humanize.naturaldelta(
+            time.mktime(end.timetuple() - time.mktime(start.timetuple())))
+
+        self.report['iterations'].append(iteration)
+
+    def dump_report(self):
+        with open(self.destination, 'w') as r:
+            json.dump(self.report, r, default=self.__convert_dates__, indent=2)
+
+    def __iter__(self):
+        for k in self.report:
+            yield k, self.report[k]
+
+    def __validate_run(self):
+        if self.report.get('params', None) is None:
+            raise Exception('No run initialized yet.')
+
+    def __convert_dates__(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.__str__()
---
+++ tests/test_reporter.py
@@ -0,0 +1,61 @@
+import unittest
+
+from antools.utils import reporter
+
+
+class TestNetworkUtils(unittest.TestCase):
+
+    def test_create_reporter(self):
+        r = reporter.Reporter('./wololo.json')
+        self.assertEqual(r.destination, './wololo.json')
+        self.assertEqual(r.report, {})
+
+    def test_spawn_run(self):
+        r = reporter.Reporter('./wololo.json')
+        r.spawn_run('Test run')
+        self.assertEqual(r.report['run_name'], 'Test run')
+        self.assertIsNotNone(r.report['start_time'])
+        self.assertEqual(r.report['params'], {})
+
+    def test_add_run_param(self):
+        r = reporter.Reporter('./wololo.json')
+        r.spawn_run('Test run')
+        r.add_run_param('test_param', 0.01)
+        r.add_run_param('test_param_2', [10, 20, 30])
+        self.assertEqual(r.report['params']['test_param'], 0.01)
+        self.assertEqual(r.report['params']['test_param_2'], [10, 20, 30])
+
+    def test_add_run_param_no_run(self):
+        r = reporter.Reporter('./wololo.json')
+
+        with self.assertRaises(Exception) as context:
+            r.add_run_param('test_param', 0.001)
+
+        self.assertTrue('No run initialized yet.' in str(context.exception))
+
+    def test_add_run_params(self):
+        r = reporter.Reporter('./wololo.json')
+        r.spawn_run('Test run')
+        r.add_run_params({'param_1': 10, 'param_2': {'test_k': 'v'}})
+        self.assertEqual(r.report['params']['param_1'], 10)
+        self.assertEqual(r.report['params']['param_2'], {'test_k': 'v'})
+
+    def test_add_run_params_no_run(self):
+        r = reporter.Reporter('./wololo.json')
+
+        with self.assertRaises(Exception) as context:
+            r.add_run_params({'param_1': 10, 'param_2': {'test_k': 'v'}})
+
+        self.assertTrue('No run initialized yet.' in str(context.exception))
+
+    def test_dict(self):
+        r = reporter.Reporter('./wololo.json')
+        r.spawn_run('Test run')
+        r.add_run_param('test_p', 100)
+        test_d = dict(r)
+        self.assertEqual(test_d['run_name'], 'Test run')
+        self.assertEqual(test_d['params'], {'test_p': 100})
+
+
+if __name__ == '__main__':
+    unittest.main()
